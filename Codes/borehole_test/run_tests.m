b25c901cf4f450194a4329fe73aab23b2ba238e8
alen <alexanderian@ncsu.edu>
2018-06-27 12:11:36 -0400

    ...

--- Codes/borehole_test/run_tests.m
+++ Codes/borehole_test/run_tests.m
@@ -25,14 +25,39 @@ set(gca, 'fontsize', 20);
 title('comparing dominant eigenvectors');
 
 
-% compare normalized eigenvalues
-l1 = lambda_grad./lambda_grad(1);
-l2 = lambda_loclin./lambda_loclin(1);
+%% compare normalized eigenvalues
+%l1 = lambda_grad./lambda_grad(1);
+%l2 = lambda_loclin./lambda_loclin(1);
+%figure;
+%bar([abs(l1(:)) abs(l2(:))]);
+%ylim([1e-16 1.5]);
+%title('comparing eigenvalues');
+%set(gca, 'yscale', 'log');
+%set(gca, 'fontsize', 20);
+%legend('alg 1.1', 'alg 1.2');
+
+% activity scores (1.1) 
+ndim = 7;
+as_grad = zeros(ndim,1);
+for i = 1:ndim
+  for j=1:1
+    as_grad(i) = as_grad(i) + lambda_grad(j).*(V(i,j).^2);
+  end
+end
+as_grad = as_grad / sum(as_grad);
+
+% activity scores (1.1) 
+as_loclin = zeros(ndim,1);
+for i = 1:ndim
+  for j=1:1
+    as_loclin(i) = as_loclin(i) + lambda_loclin(j).*(V(i,j).^2);
+  end
+end
+as_loclin = as_loclin / sum(as_loclin);
+
 figure;
-bar([abs(l1(:)) abs(l2(:))]);
-ylim([1e-16 1.5]);
-title('comparing eigenvalues');
-set(gca, 'yscale', 'log');
-set(gca, 'fontsize', 20);
+bar([as_grad(:) as_loclin(:)]);
 legend('alg 1.1', 'alg 1.2');
+set(gca, 'fontsize', 20);
+
 
